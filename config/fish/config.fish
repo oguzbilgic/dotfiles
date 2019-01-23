7b9011b58fa38014cf1da4aaa8570e386475f09f
Oguz Bilgic <fisyonet@gmail.com>
2019-01-22 23:38:29 -0800

    Move config.fish to config/fish/config.fish

---
+++ config/fish/config.fish
@@ -0,0 +1,26 @@
+# Disable fish greeting
+set fish_greeting ""
+
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_describe_style 'branch'
+set __fish_git_prompt_showcolorhints 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_show_informative_status 'yes'
+
+function fish_prompt --description 'Write out the prompt'
+  set -l last_status $status
+
+  set_color $fish_color_cwd
+  echo -n (prompt_pwd)
+  set_color normal
+
+  __fish_git_prompt
+
+  if not test $last_status -eq 0
+    set_color $fish_color_error
+  end
+
+  echo -n ' → '
+end
