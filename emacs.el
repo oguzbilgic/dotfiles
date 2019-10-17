301611272585a46aec3ceba5ea7617a966dee909
Oguz Bilgic <fisyonet@gmail.com>
2019-10-17 14:40:03 -0700

    Add parinfer to emacs

--- emacs.el
+++ emacs.el
@@ -35,6 +35,20 @@
   (set-face-foreground 'git-gutter:added "green")
   (set-face-foreground 'git-gutter:deleted "red"))
 
+;; load parinfer
+(use-package parinfer
+  :ensure t
+  :init
+  (setq parinfer-extensions
+    '(defaults       ; should be included.
+      pretty-parens  ; different paren styles for different modes.
+      evil           ; If you use Evil.
+      smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
+      smart-yank))   ; Yank behavior depend on mode.
+  (add-hook 'clojure-mode-hook #'parinfer-mode) 
+  (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+  (add-hook 'lisp-mode-hook #'parinfer-mode))
+
 ;; Set the default font
 (set-frame-font "Roboto Mono 16")
 ;; Remove the toolbar
